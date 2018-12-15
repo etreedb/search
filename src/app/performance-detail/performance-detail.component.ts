a54534294173004d7d7e587502a5d115215591af
Tom H Anderson <tom.h.anderson@gmail.com>
2018-12-15 00:16:19 -0700

    Added source to performance detail

--- src/app/performance-detail/performance-detail.component.ts
+++ src/app/performance-detail/performance-detail.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { PerformanceService } from '../performance.service';
+import { SourceService } from '../source.service';
 import { Performance } from '../performance';
+import { HalSource } from '../hal-source';
+import { Source } from '../source';
 
 @Component({
   selector: 'app-performance-detail',
@@ -12,11 +15,15 @@ import { Performance } from '../performance';
 export class PerformanceDetailComponent implements OnInit {
   private performanceId: number;
   private performance: Performance;
+  private sources: Source[];
+  public toggleSourcesFlag = false;
+  public lastSortField: string;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private performanceService: PerformanceService
+    private performanceService: PerformanceService,
+    private sourceService: SourceService
 ) { }
 
   ngOnInit() {
@@ -25,7 +32,27 @@ export class PerformanceDetailComponent implements OnInit {
 
       this.performanceService.find(this.performanceId).subscribe(data => {
         this.performance = data;
+        this.loadSources(this.performance._embedded.source._links.self.href);
       });
     });
   }
+
+  loadSources(url: string) {
+    this.sourceService.loadUrl(url).subscribe(data => {
+      this.sources = data._embedded.source;
+    });
+  }
+
+  toggleSources(): void {
+    this.toggleSourcesFlag = ! this.toggleSourcesFlag;
+  }
+
+  sort(field: string): void {
+    let modifier = 1;
+    if (this.lastSortField === field) {
+      modifier = -1;
+      this.lastSortField = '';
+    } else {
+      this.lastSortField = field;
+    }
 }
