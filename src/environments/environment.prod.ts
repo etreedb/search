1fc296f8d8e439824d5321420bbc73c203ec6c95
Tom H Anderson <tom.h.anderson@gmail.com>
2019-12-18 23:40:45 -0700

    Change live back to api.etreedb.org

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,16 +2,16 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const environment = {
   production: true,
-  apiUrl: 'https://stage.etreedb.org',
+  apiUrl: 'https://api.etreedb.org',
   title: 'search.etreedb.org',
   cacheLength: 86400,
   authConfig: <AuthConfig> {
-    issuer: 'https://stage.etreedb.org',
+    issuer: 'https://api.etreedb.org',
     redirectUri: window.location.origin + '/login-take',
     clientId: 'search.etreedb.org',
     scope: '',
     oidc: false,
     showDebugInformation: false,
-    userinfoEndpoint: 'https://stage.etreedb.org/me'
+    userinfoEndpoint: 'https://api.etreedb.org/me'
   }
 };
