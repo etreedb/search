9a3fad4fab9a2e6765c4ae3f6fbec555cfb324b0
Tom H Anderson <tom.h.anderson@gmail.com>
2020-02-09 17:59:28 -0700

    Split index search for source into id, archive identifer, md5

--- src/modules/data/service/source-checksum.service.ts
+++ src/modules/data/service/source-checksum.service.ts
@@ -3,14 +3,65 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { HalLink } from '../schema/hal-link';
 import { HalSourceChecksum } from '../schema/hal-source-checksum';
+import { HalSource } from '../schema/hal-source';
+import { environment } from '@env';
+import * as $ from 'jquery';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SourceChecksumService {
-  constructor(private http: HttpClient) { }
+  private apiUrl = environment.apiUrl;
 
-  loadLink(halLink: HalLink): Observable<HalSourceChecksum> {
+  constructor(private http: HttpClient) {}
+
+  public loadLink(halLink: HalLink): Observable<HalSourceChecksum> {
     return this.http.get<HalSourceChecksum>(halLink.href);
+  }
+
+  public search(term: string): Observable<HalSource> {
+    const query = {
+      filter: [
+        {
+          type: 'innerjoin',
+          field: 'performance',
+          alias: 'performance'
+        },
+        {
+          parentAlias: 'performance',
+          type: 'innerjoin',
+          field: 'artist',
+          alias: 'artist'
+        },
+        {
+          type: 'innerjoin',
+          field: 'sourceChecksum',
+          alias: 'sourceChecksum'
+        },
+        {
+          alias: 'sourceChecksum',
+          type: 'like',
+          field: 'description',
+          value: '%' + term + '%',
+        }
+      ],
+      'order-by': [
+        {
+          alias: 'artist',
+          type: 'field',
+          field: 'name',
+          direction: 'asc'
+        },
+        {
+          alias: 'performance',
+          type: 'field',
+          field: 'performanceDate',
+          direction: 'asc'
+        }
+      ]
+    };
+
+    const params = $.param(query);
+    return this.http.get<HalSource>(`${this.apiUrl}/source?${params}`);
   }
 }
--- src/modules/etree-db/component/index/index.component.ts
+++ src/modules/etree-db/component/index/index.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '@app/app.component';
+import { SourceChecksumService } from '@modules/data/service/source-checksum.service';
+import { HalSource } from '@modules/data/schema/hal-source';
+import { SourceService } from '@modules/data/service/source.service';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +10,61 @@ import { AppComponent } from '@app/app.component';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+  public sourceSearch = 'id';
+  public sourceSearchTerm: string;
+  public sourceSearchResult: HalSource;
+
   constructor(
-    private appComponent: AppComponent
+    private appComponent: AppComponent,
+    private sourceChecksumService: SourceChecksumService,
+    private sourceService: SourceService
    ) {
     this.appComponent.setTitle('search.etreedb.org');
    }
+
+   public searchSource() {
+    if (! this.sourceSearchTerm || ! this.sourceSearch) {
+      return;
+    }
+
+    if (this.sourceSearch === 'id') {
+      this.sourceSearchResult = null;
+
+      const query = {
+        filter: [
+          {
+            type: 'eq',
+            field: 'id',
+            value: this.sourceSearchTerm
+          }
+        ]
+      };
+
+      this.sourceService.findBy(query)
+        .subscribe(searchResult => this.sourceSearchResult = searchResult);
+    }
+
+    if (this.sourceSearch === 'archiveIdentifier') {
+      this.sourceSearchResult = null;
+
+      const query = {
+        filter: [
+          {
+            type: 'like',
+            field: 'archiveIdentifier',
+            value: '%' + this.sourceSearchTerm + '%'
+          }
+        ]
+      };
+
+      this.sourceService.findBy(query)
+        .subscribe(searchResult => this.sourceSearchResult = searchResult);
+    }
+
+    if (this.sourceSearch === 'md5') {
+      this.sourceSearchResult = null;
+      this.sourceChecksumService.search(this.sourceSearchTerm)
+        .subscribe(searchResult => this.sourceSearchResult = searchResult);
+    }
+  }
 }
